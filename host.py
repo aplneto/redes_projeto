b452f1bdf0a0fc14985dcaafc1afb976dd3e90f6
Paulino <34499237+aplneto@users.noreply.github.com>
2018-11-08 21:11:18 -0200

    Primeiro Commit
    Scripts principais do projeto

---
+++ host.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""Script do Host
+"""
+
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Created on Thu Nov  8 19:41:53 2018
+"""
+
+__authors__ = "Antônio Neto, Isac Silva, Ivan Lima, Pedro Rodolfo"
+__copyright__ = "Copyright 2018, Universidade Federal de Pernambuco"
+__credits__ = []
+
+__license__ = "MIT"
+__version__ = "alpha"
+__maintainer__ = ""
+__email__ = ["apln2@cin.ufpe.br", "its@cin.ufpe.br", "ifsl2@cin.ufpe.br",
+             "prgs@cin.ufpe.br"]
+__status__ = "development"
